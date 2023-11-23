0e201a6781350768d5bcb3d3ab28fdd85c7bb5d6
Hussain Surti <surti.huss@gmail.com>
2023-11-23 09:27:51 +0000

    chore: added documentation for listmodel

--- lib/src/listmodel.dart
+++ lib/src/listmodel.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 typedef RemovedItemBuilder<T> = Widget Function(
     T item, BuildContext context, Animation<double> animation);
 
+/// Keeps a Dart [List] in sync with an [AnimatedList].
+///
+/// The [insert] and [removeAt] methods apply to both the internal list and
+/// the animated list that belongs to [listKey].
+///
+/// This class only exposes as much of the Dart List API as is needed by the
+/// sample app. More list methods are easily added, however methods that
+/// mutate the list must make the same changes to the animated list in terms
+/// of [AnimatedListState.insertItem] and [AnimatedList.removeItem].
 class ListModel<E> {
   ListModel({
     required this.listKey,
